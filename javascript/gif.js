513a0df668bba21472fbb10de82a4763cee3b28c
singmokjai <lijason001@gmail.com>
2018-10-25 15:22:33 -0400

    added event listener

--- javascript/gif.js
+++ javascript/gif.js
@@ -1,5 +1,5 @@
 // Initial array of Gifs to pre-populate starter buttons
-var starterGifs = [corgi, nba, food, ramen, minions, hong kong, coffee];
+var starterGifs = ["corgi", "nba", "food", "ramen", "minions", "hong kong", "coffee"];
 
 function displayGifs() {
 
@@ -30,11 +30,37 @@ function displayGifs() {
         // Add gif class to button
         g.addClass("gif");
         // Add gif attribute
-        g.attr("gif-name", gif[i]);
+        g.attr("gif-name", starterGifs[i]);
         // Add text to gif button
-        g.text(gif[i]);
+        g.text(starterGifs[i]);
         // Add button to gifButtons div
         $("#gifButtons").append(g);
 
     }
-    }
+}
+
+    // Function for when add gif button is clicked
+
+    $("#add-gif").on("click", function(event) {
+        // Grap input from textbox
+        event.preventDefault();
+
+        var gif = $("#gify").val().trim();
+
+        // Add gif from textbox to starterGif array
+
+        starterGifs.push(gif);
+
+        // Call clearGifs to process array
+
+        clearGifs();
+    });
+
+    // Add event listeners with class "gif"
+
+    $(document).on("click", ".gif", displayGifs);
+
+    // Call clearGifs to display initial buttons
+
+    clearGifs();
+
